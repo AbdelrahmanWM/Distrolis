72786c1a33b2b48b05539cd379860da745246061
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-07-06 22:34:56 -0400

    chore: trying out libcurl

--- Distributed Search Engine.cpp
+++ Distributed Search Engine.cpp
@@ -1,20 +1,37 @@
-// Distributed Search Engine.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <curl/curl.h>
 #include <iostream>
 
+size_t write_callback(void* contents, size_t size, size_t nmemb, void* userp) {
+    size_t realsize = size * nmemb;
+    std::string* data = static_cast<std::string*>(userp);
+    data->append(static_cast<char*>(contents), realsize);
+    return realsize;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    CURL* curl;
+    CURLcode res;
+    std::string response;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000/api/users");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "Failed to perform HTTP request: " << curl_easy_strerror(res) << std::endl;
+        }
+        else {
+            std::cout << "Response:\n" << response << std::endl;
+        }
+        curl_easy_cleanup(curl);
+    }
+    /// my code
+
+    curl_global_cleanup();
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
