0cd138200a6a71b0802b3b2c691ea9aa9fc67e9a
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-07-10 21:37:44 -0400

    fix: fix project structure

--- include/XmlParser.h
+++ include/XmlParser.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef XML_PARSER_H
+#define XML_PARSER_H
+#include <libxml/xpath.h>
+#include<libxml/HTMLparser.h>
+#include<vector>
+#include<iostream>
+
+class XmlParser {
+public:
+	XmlParser();
+
+	~XmlParser();
+	std::vector<std::string> extractLinksFromHTML(const std::string& htmlContent);
+
+private:
+	std::string htmlContent;
+	xmlDocPtr loadHtmlDocument(const std::string& htmlContent);
+	xmlXPathContextPtr createXPathContext(xmlDocPtr doc);
+	xmlXPathObjectPtr evaluateXPathExpression(xmlXPathContextPtr xpathCtx, const std::string& xpathExpr);
+	std::vector<std::string> extractLinks(const std::string& htmlContent);
+
+};
+
+
+
+
+#endif
---
+++ source/XmlParser.cpp
@@ -0,0 +1,74 @@
+#include "../include/XmlParser.h"
+
+XmlParser::XmlParser()
+{
+    xmlInitParser();
+}
+
+XmlParser::~XmlParser()
+{
+    xmlCleanupParser();
+}
+std::vector<std::string> XmlParser::extractLinksFromHTML(const std::string& htmlContent)
+{
+    try {
+        std::vector<std::string> links = extractLinks(htmlContent);
+        for (const auto& link : links) {
+            std::cout << "Found link: " << link << std::endl;
+        }
+        return links;
+    }
+    catch (const std::exception& ex) {
+        std::cerr << "Error: " << ex.what() << std::endl;
+    }
+}
+xmlDocPtr XmlParser::loadHtmlDocument(const std::string& htmlContent) {
+    xmlDocPtr doc = htmlReadMemory(htmlContent.c_str(), htmlContent.size(), nullptr, nullptr, HTML_PARSE_RECOVER | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING);
+
+    if (doc == nullptr) {
+        throw std::runtime_error("Failed to parse HTML document.");
+    }
+    return doc;
+}
+
+xmlXPathContextPtr XmlParser::createXPathContext(xmlDocPtr doc) {
+    xmlXPathContextPtr xpathCtx = xmlXPathNewContext(doc);
+    if (xpathCtx == nullptr) {
+        throw std::runtime_error("Failed to create XPath context.");
+    }
+    return xpathCtx;
+}
+
+xmlXPathObjectPtr XmlParser::evaluateXPathExpression(xmlXPathContextPtr xpathCtx, const std::string& xpathExpr) {
+    xmlXPathObjectPtr xpathObj = xmlXPathEvalExpression(reinterpret_cast<const xmlChar*>(xpathExpr.c_str()), xpathCtx);
+    if (xpathObj == nullptr) {
+        throw std::runtime_error("Failed to evaluate xPath expression.");
+    }
+    std::cout << "XPath expression evaluated." << std::endl;
+    return xpathObj;
+}
+
+std::vector<std::string> XmlParser::extractLinks(const std::string& htmlContent) {
+    std::vector<std::string>links;
+    xmlDocPtr doc = loadHtmlDocument(htmlContent);
+    xmlXPathContextPtr xpathCtx = createXPathContext(doc);
+    std::cout << xpathCtx;
+    // Debug information about the XPath context
+    std::string xpathEpxr = "//a[@href]";
+    xmlXPathObjectPtr xpathObj = evaluateXPathExpression(xpathCtx, xpathEpxr);
+    xmlNodeSetPtr nodes = xpathObj->nodesetval;
+    for (int i = 0; i < nodes->nodeNr; ++i) {
+        xmlNodePtr node = nodes->nodeTab[i];
+        xmlChar* href = xmlGetProp(node, reinterpret_cast <const xmlChar*>("href"));
+        if (href != nullptr) {
+            std::cout << "Found link: " << href << std::endl;
+            links.push_back(reinterpret_cast<const char*> (href));
+            xmlFree(href);
+        }
+    }
+    xmlXPathFreeObject(xpathObj);
+    xmlXPathFreeContext(xpathCtx);
+    xmlFreeDoc(doc);
+    return links;
+
+}
