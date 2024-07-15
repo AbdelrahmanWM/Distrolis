a273a0ac69990506804333d277e4cc9a762f882a
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-07-15 01:18:14 -0400

    feet: implement DataBase class basic functions

--- include/DataBase.h
+++ include/DataBase.h
@@ -1,14 +1,19 @@
 #include <string>
-#include <libmongoc-1.0/mongoc/mongoc.h>
+#include<iostream>
+#include <mongoc/mongoc.h>
 #ifndef DATABASE_H
 #define DATABASE_H
 
 
 class DataBase {
 public:
-	DataBase(const std::string& connectionString);
 	~DataBase();
+	static DataBase* getInstance(const std::string& connectionString);
+	static void destroyInstance();
 private:
-	DataBase* db;
+	DataBase(const std::string& connectionString);
+	static DataBase* db;
+	mongoc_client_t*client;
+	mongoc_database_t* database;
 };
 #endif
--- source/DataBase.cpp
+++ source/DataBase.cpp
@@ -1,10 +1,50 @@
 #include "DataBase.h"
-#include <mongoc/mongoc.h>
-DataBase::DataBase(const std::string& connectionString)
-{
+#include <cstdlib>
+#include <iostream>
 
-	mongoc_cleanup()
-	if (db != nullptr) {
+DataBase* DataBase::db = nullptr;
 
-	}
+DataBase::DataBase(const std::string& connectionString) {
+    mongoc_init();
+
+    client = mongoc_client_new(connectionString.c_str());
+    if (!client) {
+        std::cerr << "Failed to create a new client instance." << std::endl;
+        return;
+    }
+
+    database = mongoc_client_get_database(client, "SearchEngine");
+
+    bson_t* command = BCON_NEW("ping", BCON_INT32(1));
+    bson_error_t error;
+    bool retval = mongoc_client_command_simple(client, "admin", command, nullptr, nullptr, &error);
+
+    if (!retval) {
+        std::cerr << "Failed to run command: " << error.message << std::endl;
+    }
+    else {
+        std::cout << "Connected successfully." << std::endl;
+    }
+
+    bson_destroy(command);
+}
+
+DataBase::~DataBase() {
+    mongoc_database_destroy(database);
+    if (client) {
+        mongoc_client_destroy(client);
+    }
+    mongoc_cleanup();
+}
+
+DataBase* DataBase::getInstance(const std::string& connectionString) {
+    if (!db) {
+        db = new DataBase(connectionString);
+    }
+    return db;
+}
+
+void DataBase::destroyInstance() {
+    delete db;
+    db = nullptr;
 }
