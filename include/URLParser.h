d2602d8d22fa624ec48b9d5db4d1cef1585716e6
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-07-21 01:38:36 -0400

    feet: add URLParser class to handle urls

---
+++ include/URLParser.h
@@ -0,0 +1,25 @@
+#ifndef URLPARSER_H
+#define URLPARSER_H
+#include<string>
+#include<regex>
+
+class URLParser {
+public:
+	URLParser(const std::string& baseURL);
+	std::string convertToAbsoluteURL(const std::string& url);
+	void setBaseURL(const std::string& baseURL);
+private:
+	std::string m_baseURL;
+
+	bool isAbsoluteURL(const std::string& url);
+	bool isProtocolRelativeURL(const std::string& url);
+	bool isRootRelativeURL(const std::string& url);
+	std::string resolveProtocolRelativeURL(const std::string& url);
+	std::string resolveRootRelativeURL(const std::string& url);
+	std::string resolvePathRelativeURL(const std::string& url);
+	bool shouldIgnoreURL(const std::string& url);
+	
+
+};
+
+#endif
