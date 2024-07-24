44bfdcd5cac21b68b9574148ee143753db79dc6d
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-07-24 18:49:06 -0400

    fix: enhance tokenization by removing long and short words

--- include/InvertedIndex.h
+++ include/InvertedIndex.h
@@ -13,17 +13,21 @@
 class InvertedIndex
 {
 public:
-    InvertedIndex(const DataBase *&db);
-    void run();
+    InvertedIndex(const DataBase *&db,const std::string& database_name, const std::string& collection_name,const std::string& documents_collection_name);
+    void run(bool clear = false);
     void addDocument(const std::string docId, std::string &content);
 
 private:
     std::unordered_map<std::string, std::vector<Posting>> m_index;
     const DataBase *&m_db;
+    const std::string m_database_name;
+    const std::string m_collection_name;
+    const std::string m_documents_collection_name;
     std::vector<std::string> tokenize(std::string &content);
     std::string normalize(std::string &token);
     std::string stem(std::string &word);
     bool isStopWord(std::string &word);
+    static const std::unordered_set<std::string>& getStopWords();
 };
 
 #endif
--- source/InvertedIndex.cpp
+++ source/InvertedIndex.cpp
@@ -1,18 +1,21 @@
 #include "InvertedIndex.h"
 #include "DataBase.h"
 
-InvertedIndex::InvertedIndex(const DataBase *&db)
-    : m_index({}), m_db(db)
+InvertedIndex::InvertedIndex(const DataBase *&db,const std::string& database_name, const std::string& collection_name,  const std::string& documents_collection_name)
+    : m_index{}, m_db{db}, m_database_name{database_name},m_collection_name{collection_name},m_documents_collection_name{documents_collection_name}
 {
 }
 
-void InvertedIndex::run()
+void InvertedIndex::run(bool clear)
 {
     std::vector<bson_t> documents{};
 
     try
-    {
-        documents = m_db->getAllDocuments();
+    {   
+        if(clear){
+            m_db->clearCollection(m_database_name,m_collection_name);
+        }
+        documents = m_db->getAllDocuments(m_database_name,m_documents_collection_name);
         std::cout << "size: " << documents.size() << '\n';
         for (const auto &document : documents)
         {
@@ -20,7 +23,7 @@ void InvertedIndex::run()
             std::string docId = m_db->extractIndexFromIndexDocument(document);
             addDocument(docId, content);
         }
-        m_db->saveInvertedIndex(m_index);
+        m_db->saveInvertedIndex(m_index,m_database_name,m_collection_name);
     }
     catch (std::exception &ex)
     {
@@ -37,6 +40,7 @@ void InvertedIndex::addDocument(const std::string docId, std::string &content)
         if (isStopWord(token))
             continue;
         token = stem(token);
+        std::cout<<"* "<<token<<"\n";
         m_index[token].push_back({docId, 1});
     }
 }
@@ -48,6 +52,7 @@ std::vector<std::string> InvertedIndex::tokenize(std::string &content)
     std::vector<std::string> tokens;
     while (stream >> token)
     {
+        if(token.size()>3&&token.size()<=20)// Temporary filtration step until better solution is found
         tokens.push_back(token);
     }
     return tokens;
@@ -84,6 +89,30 @@ std::string InvertedIndex::stem(std::string &word)
 
 bool InvertedIndex::isStopWord(std::string &word)
 {
-    static const std::unordered_set<std::string> stopWords = {"the", "and", "is", "in", "at", "of", "on"};
-    return stopWords.find(word) != stopWords.end();
+    
+    return InvertedIndex::getStopWords().find(word) !=InvertedIndex::getStopWords().end();
+}
+
+const std::unordered_set<std::string> &InvertedIndex::getStopWords()
+{
+    static const std::unordered_set<std::string> stopWords = {
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", 
+    "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", 
+    "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", 
+    "each", "few", "for", "from", "further", 
+    "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", 
+    "how", "how's", 
+    "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", 
+    "let's", 
+    "me", "more", "most", "mustn't", "my", "myself", 
+    "no", "nor", "not", 
+    "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", 
+    "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", 
+    "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", 
+    "under", "until", "up", 
+    "very", 
+    "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", 
+    "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
+};
+return stopWords;
 }
