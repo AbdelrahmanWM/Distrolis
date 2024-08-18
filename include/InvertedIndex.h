8423054d680c408bbc847ea61563d78f273c5925
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-08-18 19:26:31 -0400

    feat: separate invertedIndex term documents

--- include/InvertedIndex.h
+++ include/InvertedIndex.h
@@ -17,8 +17,10 @@ public:
     InvertedIndex(const DataBase *&db,const std::string& database_name, const std::string& collection_name,const std::string& documents_collection_name);
     void run(bool clear = false);
     void addDocument(const std::string docId, std::string &content);
+    void retrieveExistingIndex();
 
 private:
+    void extractInvertedIndexDocument(const bson_t*document);
     std::unordered_map<std::string, std::unordered_map<std::string,std::vector<int>>> m_index;
     const DataBase *&m_db;
     const std::string m_database_name;
--- source/InvertedIndex.cpp
+++ source/InvertedIndex.cpp
@@ -9,22 +9,27 @@ InvertedIndex::InvertedIndex(const DataBase *&db,const std::string& database_nam
 
 void InvertedIndex::run(bool clear)
 {
-    std::vector<bson_t> documents{};
+    std::vector<bson_t*> documents{};
 
     try
-    {   
-        if(clear){
-            m_db->clearCollection(m_database_name,m_collection_name);
+    {  
+        // m_index = m_db -> getDocument(m_database_name,m_collection_name);
+        
+
+        if(!clear){
+            retrieveExistingIndex();
         }
         documents = m_db->getAllDocuments(m_database_name,m_documents_collection_name);
         std::cout << "size: " << documents.size() << '\n';
-        for (const auto &document : documents)
+        for (auto document : documents)
         {
             std::string content = m_db->extractContentFromIndexDocument(document);
             std::string docId = m_db->extractIndexFromIndexDocument(document);
             addDocument(docId, content);
         }
-        m_db->saveInvertedIndex(m_index,m_database_name,m_collection_name);
+        m_db->clearCollection(m_database_name,m_collection_name);// clear older documents
+        m_db->saveInvertedIndex(m_index,m_database_name,m_collection_name); // save updated documents
+
     }
     catch (std::exception &ex)
     {
@@ -49,12 +54,52 @@ void InvertedIndex::addDocument(const std::string docId, std::string &content)
     }
 }
 
+void InvertedIndex::retrieveExistingIndex()
+{
+    std::vector<bson_t*> documents = m_db->getAllDocuments(m_database_name,m_collection_name);
 
+    for(const bson_t* document:documents){
+        InvertedIndex::extractInvertedIndexDocument(document);
+    }
 
+}
 
+void InvertedIndex::extractInvertedIndexDocument(const bson_t *document)
+{
+    bson_iter_t iter;
+    if(!bson_iter_init(&iter,document)){
+        std::cerr<<"Failed to initialize BSON iterator."<<std::endl;
+        return;
+    }
+    while(bson_iter_next(&iter)){
+        if(BSON_ITER_HOLDS_ARRAY(&iter)){
+            std::string term = bson_iter_key(&iter);
+            std::unordered_map<std::string,std::vector<int>> doc_map;
+            bson_iter_t array_iter;
+            bson_iter_recurse(&iter,&array_iter);
+            while(bson_iter_next(&array_iter)){
+                if(BSON_ITER_HOLDS_DOCUMENT(&array_iter)){
+                    bson_iter_t doc_iter;
+                    bson_iter_recurse(&array_iter,&doc_iter);
 
-
-
-
-
-
+                    std::string doc_id;
+                    std::vector<int> positions{};
+                    doc_id = bson_iter_key(&array_iter);
+                    while(bson_iter_next(&doc_iter)){
+                        if(strcmp(bson_iter_key(&doc_iter),"docId") == 0){
+                            doc_id = bson_iter_utf8(&doc_iter,nullptr);
+                        }else if(strcmp(bson_iter_key(&doc_iter),"positions") == 0){
+                            bson_iter_t positions_iter;
+                            bson_iter_recurse(&doc_iter,&positions_iter);
+                            while(bson_iter_next(&positions_iter)){
+                                positions.push_back(bson_iter_int32(&positions_iter));
+                            }
+                        }
+                    }
+                    doc_map[doc_id]=positions;
+                }
+            }
+            m_index[term] = doc_map;
+        }
+    }
+}
